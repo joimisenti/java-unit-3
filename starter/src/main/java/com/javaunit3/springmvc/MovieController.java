dbd7474cc20d3ef58158dafe22d5b473dc25d54c
Joi Misenti <jmisenti@Jois-MacBook-Air.local>
2023-09-04 16:54:56 -0400

    working on html pages

---
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,20 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MovieController {
+
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+            return "bestMovie";
+    }
+}
